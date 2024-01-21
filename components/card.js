8d5d1a38a0e094ca362b6687283261e452577f8b
Bryan Emerson <bryan.l.emerson@gmail.com>
2024-01-20 20:25:13 -0800

    card and tech display

--- components/card.js
+++ components/card.js
@@ -1,28 +1,54 @@
-import Image from 'next/image'
-import Link from '@mui/material/Link';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
+import React from "react";
+import Image from "next/image";
+import Link from "@mui/material/Link";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
 
-import styles from '@/styles/card.module.css'
+import styles from "@/styles/card.module.css";
+import Chip from "@mui/material/Chip";
 
 function Card(props) {
-  console.log("hello world")
   return (
     <>
-    <div className={styles.card}>
-      <Image src={props.projObj.image} alt="screenshot of app" width={600} height={356} disableGutters={true} />
-      <Typography>
-        {props.projObj.stack}
-      </Typography>
-      <Typography variant="h4">
-        {props.projObj.name}
-      </Typography>
-      <Typography >
-      <Link href={props.projObj.git} underline="hover" sx={{ color: "black" }}>Read More</Link>
-      </Typography>
-    </div>
+      <div className={styles.card}>
+        <Image
+          src={props.projObj.image}
+          alt="screenshot of app"
+          width={600}
+          height={356}
+          disablegutters
+        />
+        <Box
+          sx={{
+            display: "flex", // Enables flexbox layout
+            flexWrap: "wrap", // Allows items to wrap onto multiple lines
+            alignItems: "center", // Aligns items vertically in the center
+            gap: 1, // Adds a gap between items
+          }}
+        >
+          {props.projObj.stack.map((tech, index) => (
+            <React.Fragment key={index}>
+              <Typography>{tech}</Typography>
+              {index < props.projObj.stack.length - 1 && (
+                <Typography sx={{ mx: 0.2 }}>/</Typography>
+              )}
+            </React.Fragment>
+          ))}
+        </Box>
+        <Typography variant="h4">{props.projObj.name}</Typography>
+        <Typography>
+          <Link
+            href={props.projObj.git}
+            underline="hover"
+            sx={{ color: "black" }}
+          >
+            Read More
+          </Link>
+        </Typography>
+      </div>
     </>
-  )
+  );
 }
 
 export default Card;
--- components/work.js
+++ components/work.js
@@ -28,15 +28,15 @@ function Work() {
       info: 'This To Do List App was created with TypeScript, HTML, and CSS. It was bundled with snowpack. New Task <li> items are programatically appended to the <ul> parent node and are written to local storage for persistence. The app can clear checked tasks by comparing unique IDs between items and splicing them out of the Tasks array, then re writing it to local storage. The list is then re rendered in the browser to reflect the removed items!',
       url: 'https://soft-clock.surge.sh/',
       git: 'https://github.com/bryan-emerson/toDoList-Snowpack',
-      stack: 'Typescript/Snowpack',
+      stack: ['Typescript', 'Next.js', 'Material UI', 'React', 'Recoil', 'Axios', 'Vercel'],
       image: KMM
     },
     {
-      name: 'Weather App!',
+      name: 'Laborie',
       info: 'In this React app, the useEffect hook calls navigator.geolocation.getCurrentPosition() when the page loads to determine the user’s location. A fetch call is then made to the Open Weather API, and hooks are used to set the returned data to state. Conditional rendering shows a loading message until the weather app UI renders.  A link at the bottom of the app will programmatically make a search string for the dark skies website, and take users to a more in-depth weather data experience.',
       url: 'https://unused-earthquake.surge.sh/',
       git: 'https://github.com/bryan-emerson/weather-one',
-      stack: 'React/Bootstrap',
+      stack: ['C#', '.Net', 'React', 'Azure', 'SQL'],
       image: Laborie
     },
     {
@@ -44,7 +44,7 @@ function Work() {
       info: 'This is a single page React app using functional component based architecture. Hooks and Props are used for state management. The useEffect hook makes an intial fetch call to the Dad Jokes API. These initial jokes are rendered until the user enters a search term in the form. The form input is used to programatically update the fetch call on change, and new jokes matching the search term are rendered!',
       url: 'https://abhorrent-lumber.surge.sh/',
       git: 'https://github.com/bryan-emerson/jokeDad',
-      stack: 'React/Bootstrap',
+      stack: ['React', 'Bootstrap'],
       image: Dad2
     },
     {
@@ -52,7 +52,7 @@ function Work() {
       info: 'This project is a simple game of memory. Click on cards to find matches! The project was built with a focus on functional programming as opposed to object oriented programming. Vanilla Javascript & CSS were used along with DOM manipulation to create interactive gameplay with the HTML elements.',
       url: 'https://overt-library.surge.sh',
       git: 'https://github.com/bryan-emerson/memory',
-      stack: 'Javascript/HTML/CSS',
+      stack: ['Javascript', 'HTML', 'CSS'],
       image: Memory
     },
   ]
