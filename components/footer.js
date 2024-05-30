27d50bcd37e5ff2c61ff8e3b693f6e93c4719a2d
Bryan Emerson <bryan.l.emerson@gmail.com>
2024-05-30 14:43:59 -0700

    character escape

--- components/footer.js
+++ components/footer.js
@@ -92,8 +92,8 @@ function Footer() {
         justifyContent: 'center',
         flex: { xs: '1 1 auto', md: '1 1 30%' }
       }}>
-        <Typography variant="h6">Think We'd Be A Great Fit?</Typography>
-        <Typography variant="h6">Let's Talk...</Typography>
+        <Typography variant="h6">Think We&apos;d Be A Great Fit?</Typography>
+        <Typography variant="h6">Let&apos;s Talk...</Typography>
       </Container>
       <Divider orientation="vertical" flexItem sx={{ bgcolor: '#FFFFFF', my: 2, display: { xs: 'none', md: 'block' } }} />
       <Container sx={{
